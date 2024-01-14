b8be9dc2f36d73a1552815f7580012b6cf26f736
Henrique Rocha <81716008+Rochadevj@users.noreply.github.com>
2024-01-14 01:52:11 -0300

    Update auxiliar.py

--- auxiliar.py
+++ auxiliar.py
@@ -3,15 +3,3 @@ import pyautogui
 
 time.sleep(2)
 print(pyautogui.position())
-
-        # codigo       marca  ... custo               obs
-#0    MOLO000251    Logitech  ...   6.5               NaN
-#1    MOLO000192    Logitech  ...   5.0               NaN
-#2    CAHA000251     Hashtag  ...  11.0               NaN
-#3    CAHA000252     Hashtag  ...  11.0  Conferir estoque
-#4    MOMU000111  Multilaser  ...   3.4               NaN
-#288  ACAP000192       Apple  ...   3.8               NaN
-#289  ACSA0009.3     Samsung  ...   2.1               NaN
-#290  CEMO000271    Motorola  ...  72.5               NaN
-#291  FOMO000152    Motorola  ...  33.0               NaN
-#292  CEMO000223    Motorola  ...  55.0               NaN
